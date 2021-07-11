2d7b5d27f5ceda2a18f011bbf57c083f44c56959
voron521 <voron521@mail.ru>
2021-07-11 13:16:01 +0300

    lesson_5_complete

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -18,12 +18,12 @@ from .views import index, contacts
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
     path('contacts/', contacts, name="contacts"),
     path('products/', include('mainapp.urls', namespace='products')),
+    path('basket/', include('basketapp.urls', namespace='basket')),
     path('auth/', include('authapp.urls', namespace='auth')),
 ]
 
