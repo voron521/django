977a2eb0765ee7ca8cacce50149f86d3af94ebc8
voron521 <voron521@mail.ru>
2021-07-30 18:13:02 +0300

    lesson_2_2

--- geekshop/authapp/admin.py
+++ geekshop/authapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from authapp.models import ShopUser
+
+from authapp.models import ShopUser, ShopUserProfile
 
 admin.site.register(ShopUser)
-
-# Register your models here.
+admin.site.register(ShopUserProfile)
