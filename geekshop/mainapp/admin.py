4ba3b5eef150af63b608d764d9620dd053130fbf
voron521 <voron521@mail.ru>
2021-08-16 20:17:16 +0300

    less_2_4 complete

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-
 from .models import ProductCategory, Product
 
 admin.site.register(ProductCategory)
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'price',
+        'quantity',
+        'created',
+    ]
