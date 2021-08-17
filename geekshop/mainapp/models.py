5da681d6bc082557ae7895ecd71a5316eb4651bc
voron521 <voron521@mail.ru>
2021-08-17 13:22:54 +0300

    lesson_2_5 complete

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+
 class ProductCategory(models.Model):
-    name = models.CharField(verbose_name = 'имя', max_length=64, unique = True)
-    description = models.TextField(verbose_name = 'описание', blank=True)
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name or f"Category with id - {self.pk}"
@@ -15,57 +18,56 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-
     category = models.ForeignKey(
         ProductCategory,
-        on_delete = models.CASCADE,
+        on_delete=models.CASCADE,
         verbose_name='категория',
-
-
     )
     name = models.CharField(
-        verbose_name = 'имя продукта',
+        verbose_name='имя продукта',
         max_length=128,
-
     )
-
     image = models.ImageField(
-        upload_to = 'product_images',
-        blank = True,
+        upload_to='product_images',
+        blank=True,
         verbose_name='изображение',
     )
-
     short_desc = models.CharField(
-
         verbose_name='краткое описание',
         max_length=100,
         blank=True,
     )
-
     description = models.TextField(
-        verbose_name = 'описание',
-        blank=True
+        verbose_name='описание',
+        blank=True,
     )
-
     price = models.DecimalField(
         verbose_name='цена',
-        max_digits = 8,
-        decimal_places = 2,
-        default = 0,
+        max_digits=8,
+        decimal_places=2,
+        default=0,
     )
-
     quantity = models.PositiveIntegerField(
         verbose_name='количество товара на складе',
-        default = 0,
-
+        default=0,
     )
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated = models.DateTimeField(
+        auto_now=True,
+    )
+
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name or f"Product with id - {self.pk}"
 
+    @staticmethod
+    def get_items():
+        return Product.objects.filter(is_deleted=False).order_by('category', 'name')
+
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
